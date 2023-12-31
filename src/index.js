9b52d414aec927c65ec6c7d2ef5dd9476e33d0bf
akram-kh21 <154263780+akram-kh21@users.noreply.github.com>
2023-12-31 16:06:00 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -76,6 +76,112 @@ client.on("messageCreate", (msg) => {
     });
     msg.channel.send({ embeds: [embed] });
   }
+    //kick command
+if (command === "kick") {
+  const member =
+    msg.mentions.members.first() ||
+    msg.guild.members.cache.get(args[0]) ||
+    msg.guild.members.cache.find(
+      (x) =>
+        x.user.username.toLowerCase() ===
+        args.join(" " || x.username === args[0])
+    );
+  if (!msg.member.permissions.has(PermissionsBitField.Flags.KickMembers))
+    return msg.channel.send("You don't have permission to kick members");
+  if (!member) return msg.channel.send("Please mention a user");
+  if (msg.member === member)
+    return msg.channel.send("You can't kick yourself");
+  if (!member.kickable) return msg.channel.send("I can't kick that member");
+  let reason = args.slice(1).join(" ") || "No reason given";
+  const embed = new EmbedBuilder()
+    .setColor("Random")
+    .setDescription(
+    `:white_check_mark: **${member.user.tag} has been kicked for ${reason}**`
+    );
+    const dmembed = new EmbedBuilder()
+    .setColor("Random")
+    .setDescription(
+      `:x: **You were kicked from ${msg.guild.name} by ${msg.author.tag} for ${reason}**`
+    );
+  member.send({ embeds: [dmembed] }).catch((err) => {
+    console.log(`${member.user.tag} was not kicked due to ${err}`);
+  });
+  member.kick().catch((err) => {
+    msg.channel.send(`${member.user.tag} was not kicked due to ${err}`);
+  });
+  msg.channel.send({ embeds: [embed] });
+}
+
+//mute command
+if (command === "mute") {
+  const member =
+    msg.mentions.members.first() ||
+    msg.guild.members.cache.get(args[0]) ||
+    msg.guild.members.cache.find(
+      (x) =>
+        x.user.username.toLowerCase() ===
+        args.join(" " || x.username === args[0])
+    );
+  if (!msg.member.permissions.has(PermissionsBitField.Flags.ModerateMembers))
+    return msg.channel.send("You don't have permission to mute members");
+  if (!member) return msg.channel.send("Please mention a user");
+  if (msg.member === member)
+    return msg.channel.send("You can't mute yourself");
+  if (!member.manageable) return msg.channel.send("I can't mute that member");
+  let reason = args.slice(1).join(" ") || "No reason given";
+  const embed = new EmbedBuilder()
+    .setColor("Random")
+    .setDescription(
+    `:white_check_mark: **${member.user.tag} has been muted for ${reason}**`
+    );
+    const dmembed = new EmbedBuilder()
+    .setColor("Random")
+    .setDescription(
+      `:x: **You were muted from ${msg.guild.name} by ${msg.author.tag} for ${reason}**`
+    );
+  member.send({ embeds: [dmembed] }).catch((err) => {
+    console.log(`${member.user.tag} was not muted due to ${err}`);
+  });
+  member.timeout(1000).catch((err) => {
+    msg.channel.send(`${member.user.tag} was not muted due to ${err}`);
+  });
+  msg.channel.send({ embeds: [embed] });
+}
+//unmute command
+if (command === "unmute") {
+  const member =
+    msg.mentions.members.first() ||
+    msg.guild.members.cache.get(args[0]) ||
+    msg.guild.members.cache.find(
+      (x) =>
+        x.user.username.toLowerCase() ===
+        args.join(" " || x.username === args[0])
+    );
+  if (!msg.member.permissions.has(PermissionsBitField.Flags.ModerateMembers))
+    return msg.channel.send("You don't have permission to unmute members");
+  if (!member) return msg.channel.send("Please mention a user");
+  if (msg.member === member)
+    return msg.channel.send("You can't unmute yourself");
+  if (!member.manageable) return msg.channel.send("I can't unmute that member");
+  let reason = args.slice(1).join(" ") || "No reason given";
+  const embed = new EmbedBuilder()
+    .setColor("Random")
+    .setDescription(
+    `:white_check_mark: **${member.user.tag} has been unmuted for ${reason}**`
+    );
+    const dmembed = new EmbedBuilder()
+    .setColor("Random")
+    .setDescription(
+      `:x: **You were unmuted from ${msg.guild.name} by ${msg.author.tag} for ${reason}**`
+    );
+  member.send({ embeds: [dmembed] }).catch((err) => {
+    console.log(`${member.user.tag} was not unmuted due to ${err}`);
+  });
+  member.timeout(null).catch((err) => {
+    msg.channel.send(`${member.user.tag} was not unmuted due to ${err}`);
+  });
+  msg.channel.send({ embeds: [embed] });
+}
 });
 
 client.login(process.env.TOKEN);
